2b12ad21c44abdd9e9c2bf8097ca96ae087287f2
carolinmimmie <carolinmimmie@gmail.com>
2023-10-05 13:01:56 +0200

    Add ability to add movie

--- Domain/Movie.cs
+++ Domain/Movie.cs
@@ -17,8 +17,8 @@ class Movie
 
     [MaxLength(50)]
     public required string Director { get; set; }
-    
-    public required DateTime ReleaseYear { get; set; }
+
+    public required int ReleaseYear { get; set; }
 
     // Navigation property
     public ICollection<Actor> Actors { get; set; } = new List<Actor>();
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using IMDb.Domain;
 using static System.Console;
 
@@ -6,17 +7,10 @@ namespace IMDb;
 
 class Program
 {
-
-    // Httpclient finns tillgänglig, behöver inte installeras via NuGet
-    //Den gör HTTP anrop över nätverket
-    //För att göra HHTP.anrop ( text GET) behöver vi använda ett bibliotek
-    //som kan göra detta. såsom HttpClient. Använder detta i våran metod GetForCast()
-
     static readonly HttpClient httpClient = new()
     {
-        BaseAddress = new Uri("https://localhost:8000/")//adressen
+        BaseAddress = new Uri("https://localhost:8000/")
     };
-
 
     public static void Main()
     {
@@ -56,55 +50,45 @@ class Program
 
     private static void ListMoviesView()
     {
-        // 1 - Hämta movies från backend (web api) (alltså skicka en HHTP GET förfrågan)
         var movies = GetMovies();
 
-        // 2 - Skriv ut en "tabell", på samma sätt som vi precis gjorde i vår web-applikation
         Write($"{"Titel",-16}");
-        Write($"{"Genre",-16}");
+        Write($"{"Genre",-26}");
         WriteLine("Director");
 
         foreach (var movie in movies)
         {
             Write($"{movie.Title,-16}");
-            Write($"{movie.Genre,-16}");
+            Write($"{movie.Genre,-26}");
             WriteLine(movie.Director);
         }
 
-        // 3 - Vänta på att användaren trycker på escape, återvänd då till huvudmenyn
         WaitUntilKeyPressed(ConsoleKey.Escape);
     }
 
-
-
-    // Vi vill Returnera IEnumerable <Movie>
-    // IEnumarable inte är en datatyp i sig själv utan snarare
-    // ett gränssnitt som används för att hantera upprepning över samlingar av data.
-
     private static IEnumerable<Movie> GetMovies()
     {
-        // TODO Hämta samtliga filmer genom att skicka ett HTTP GET till https://localhost:8000/movies
+        // TODO HTTP GET till https://localhost:8000/movies
 
-        // 1 - Skicka ett HTTP GET-anrop till backend (web api)
-        var response = httpClient.GetAsync("movies").Result;
+        // "https://localhost:8000/" + "movies" => https://localhost:8000/movies
+        var response = httpClient.GetAsync("movies")
+            .Result;
 
-        // 2 - Läs ut JSON som vi fått tillbaka
-        var json = response.Content.ReadAsStringAsync().Result;
+        var json = response.Content
+            .ReadAsStringAsync()
+            .Result;
 
         var serializeOptions = new JsonSerializerOptions
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase
         };
 
-        // 3 - Deserialisera JSON till ett objekt (IEnumerable<WeatherForecast>)
+        // Deserialisera från JSON till IEnumerable<Movie>
         var movies = JsonSerializer
             .Deserialize<IEnumerable<Movie>>(json, serializeOptions)
             ?? new List<Movie>();
 
-        // 4 - Returnera resultatet (IEnumerable<Movie>)
         return movies;
-
-        throw new NotImplementedException();
     }
 
     private static void AddMovieView()
@@ -113,7 +97,7 @@ class Program
         var plot = GetUserInput("Handling");
         var director = GetUserInput("Regissör");
         var genre = GetUserInput("Genre");
-        var releaseYear = DateTime.Parse(GetUserInput("Premiär (YYYY-MM-DD)"));
+        var releaseYear = int.Parse(GetUserInput("År"));
 
         var movie = new Movie
         {
@@ -145,10 +129,23 @@ class Program
         // TODO Skicka information om filmen till web API:et genom att skicka 
         //      ett HTTP POST-anrop till https://localhost:8000/movies
 
-        // Om statuskod är "400 Bad Request", kasta exception
+        // 1 - Serialisera movie-objekt till JSON ({ "title": "Aliens", "plot": "Lorem ipsum dolor", ... })
+        var json = JsonSerializer.Serialize(movie);
 
-        // Ta bort
-        throw new NotImplementedException();
+        var body = new StringContent(
+          json,
+          Encoding.UTF8,
+          // Beskriver formatet på data
+          "application/json");
+
+        // POST https://localhost:8000/movies
+        var response = httpClient.PostAsync("movies", body).Result;
+
+        // Om statuskod är "400 Bad Request", kasta exception som du sedan fångar
+        // där SaveMovie() anropas, och då visar meddelandet "Ogiltig information" i 2 sekunder.
+
+        // Kasta exception om statuskoden inte ligger inom 2xx-omfånget.
+        response.EnsureSuccessStatusCode();
     }
 
     private static string GetUserInput(string label)
